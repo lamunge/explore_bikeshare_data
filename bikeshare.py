3118682105e9d29958756a43481c6a75b9dacefa
Anna Redgrave <anna.redgrave93@gmail.com>
2018-03-23 00:28:04 -0400

    calculates bikeshare data metrics based on user selection of city and date

--- bikeshare.py
+++ bikeshare.py
@@ -6,24 +6,21 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def bikeshare():
+    data_metrics = {}
     if request.form: #makes sure it is a "POST" request and prevents 400 error
         city = request.form['city']
         month = request.form['month']
         day = request.form['day']
-
-        chicago = pd.read_csv("chicago.csv")
-        new_york_city = pd.read_csv("new_york_city.csv")
-        washington = pd.read_csv("washington.csv")
 
         #filter data by city
         if city == "chicago":
             data = chicago
         elif city == "new york city":
             data = new_york_city
-        elif city == "washington":
+        elif city == "washington dc":
             data = washington
         else:
-            data = pd.concat([chicago, new_york_city, washington], ignore_index=True)
+            data = pd.concat([chicago, new_york_city, washington], join='outer', ignore_index=True)
 
         #convert start and end times to Timestamp
         data["Start Time"] = data["Start Time"].apply(lambda x: pd.Timestamp(x))
@@ -48,9 +45,31 @@ def bikeshare():
             day_mask = [any(tup) for tup in zip(start_mask, end_mask)]
             data = data.loc[day_mask]
 
-    return render_template("index.html")
+        #collect data metrics
+        data_metrics['city'] = city
+        data_metrics['month'] = month
+        data_metrics['day'] = day
+        data_metrics["total trips"] = data.shape[0]
+        data_metrics["total hours"] = round(data["Trip Duration"].sum()/3600, 1) #trip duration is in seconds
+        data_metrics["average minutes per trip"] = round((data["Trip Duration"].sum()/60)/data_metrics["total trips"], 0)
+        data_metrics["most popular start station"] = data["Start Station"].mode()[0]
+        data_metrics["most popular end station"] = data["End Station"].mode()[0]
+        data_metrics["most popular trip"] = data["Start Station"].str.cat(data["End Station"], sep=" to ").mode()[0]
+        data_metrics["number subscribers"] = data[data["User Type"] == "Subscriber"]["User Type"].count()
+        data_metrics["number customers"] = data[data["User Type"] == "Customer"]["User Type"].count()
+        if "Gender" in data.columns:
+            data_metrics["number males"] = data[data["Gender"] == "Male"]["User Type"].count()
+            data_metrics["number females"] = data[data["Gender"] == "Female"]["User Type"].count()
+            data_metrics["percent reporting gender"] = round((data_metrics["number males"] + data_metrics["number females"])/data_metrics["total trips"]*100, 2)
+        if "Birth Year" in data.columns:
+            data_metrics["most popular birth year"] = int(data["Birth Year"].mode()[0])
+
+    return render_template("index.html", data_metrics=data_metrics)
 
 if __name__ == '__main__':
+    chicago = pd.read_csv("chicago.csv")
+    new_york_city = pd.read_csv("new_york_city.csv")
+    washington = pd.read_csv("washington.csv")
     #init_gui(app)
 
     app.run(debug=True)
